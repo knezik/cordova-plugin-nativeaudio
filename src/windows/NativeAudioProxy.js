8bf57ed1ff3fd4ca998794772510bd4167b454d0
Jan Knezik <knezik@ginasystem.com>
2015-10-21 22:23:43 +0200

    Rewritten to C#

--- src/windows/NativeAudioProxy.js
+++ src/windows/NativeAudioProxy.js
@@ -1,179 +1,90 @@
-var OPT_FADE_MUSIC = "fadeMusic";
-var _fadeMusic = false; 
-
-var _graph = null;
-var _deviceOutput = null;
-
-
-var _fileInputs = {};
-
-function addFileInput(id, node, volume, voices, delay) {
-    if (!_fileInputs[id]) {    
-        _fileInputs[id] = {};
-        _fileInputs[id].sound = result.fileInputNode;
-        
-        _fileInputs[id].settings = {};
-        _fileInputs[id].settings.volume = volume;
-        _fileInputs[id].settings.voices = voices;                    
-        _fileInputs[id].settings.delay = delay;
-        
-        return true; 
-    } else {
-        handleError('file with id ' + id + ' is already loaded');
-        return false;
-    }
-}
-
-
-function handleError(msg) {
-    console.log('NativeAudioProxy: ' + msg);
-}
-
-
-function createAudioGraphAsync() {
-    var settings = new Windows.Media.Audio.AudioGraphSettings();
-    
-    return Windows.Media.Audio.AudioGraph.createAsync(settings).then(
-        function (result) {
-            _graph = result.graph;    
-            return _graph.createDeviceOutputNodeAsync(Windows.Media.Render.AudioRenderCategory.backgroundCapableMedia);
-        }
-    ).then(
-        function (result) {
-            _deviceOutput = result.deviceOutput;
-            _graph.ResetAllNodes();
-            _graph.Start();
-        }
-    );
-}
-
-
-
 module.exports = {
     
     setOptions: function(successCallback, errorCallback, options) {
-        if (options[OPT_FADE_MUSIC])
-            _fadeMusic = options[OPT_FADE_MUSIC];
+        var res = NativeAudioRT.NativeAudioRTImpl.setOptions(options);
+        
+        if (res == 0) {
+            errorCallback(res);
+        }
+        else {
+            successCallback(res);
+        }
     },
 
     preloadSimple: function(successCallback, errorCallback, id, assetPath) {
-        // 
-            var finalize = function () {
-                _graph.createFileInputNodeAsync(assetPath).done(
-                    function (result) {  
-                        if (addFileInput(id, result.fileInputNode, 1, 1, 0)) {
-                            _fileInputNode.Stop();
-                            _fileInputNode.AddOutgoingConnection(_deviceOutput);
-                            successCallback();
-                        } else {
-                            errorCallback('file with id ' + id + ' is already loaded');
-                        }
-                    },
-                    function (e) { handleError('error preloading file ' + e);  errorCallback(e); }
-                );
-            }
-        //  ^
-        //  |
+        var res = NativeAudioRT.NativeAudioRTImpl.preloadSimple(id, assetPath);
         
-        if (_deviceOutput === null) {
-            createAudioGraphAsync().done(
-                finalize,
-                function (e) { handleError('error preloading file ' + e);  errorCallback(e); }
-            );
+        if (res == 0) {
+            errorCallback(res);
+        }
+        else {
+            successCallback(res);
         }
     },
 
     preloadComplex: function(successCallback, errorCallback, id, assetPath, volume, voices, delay) {
-        //
-            var finalize = function () {
-                _graph.createFileInputNodeAsync(assetPath).done(
-                    function (result) {  
-                        if (addFileInput(id, result.fileInputNode, volume, voices, delay)) {
-                            _fileInputNode.Stop();
-                            _fileInputNode.AddOutgoingConnection(_deviceOutput);
-                            successCallback();
-                        } else {
-                            errorCallback('file with id ' + id + ' is already loaded');
-                        }     
-                    },
-                    function (e) { handleError('error preloading file ' + e);  errorCallback(e); }
-                );
-            }
-        //  ^
-        //  |
+        var res = NativeAudioRT.NativeAudioRTImpl.preloadComplex(id, assetPath, volume, voices, delay);
         
-        if (_deviceOutput === null) {
-            createAudioGraphAsync().done(
-                finalize,
-                function (e) { handleError('error preloading file ' + e);  errorCallback(e); }
-            );
+        if (res == 0) {
+            errorCallback(res);
+        }
+        else {
+            successCallback(res);
         }
     },
     
     play: function(successCallback, errorCallback, id) {
-        if (_fileInputs[id]) {
-            var sound = _fileInputs[id].sound;
-            var settings = _fileInputs[id].settings;
-            
-            sound.outgoingGain = settings.volume;
-            sound.seek(settings.delay);
-            sound.start();
-            
-            successCallback();
-        } else {
-            handleError('file with id ' + id + ' is not loaded');  
-            errorCallback('file with id ' + id + ' is not loaded');
+        var res = NativeAudioRT.NativeAudioRTImpl.play(id);
+        
+        if (res == 0) {
+            errorCallback(res);
+        }
+        else {
+            successCallback(res);
         }
     },
     
     loop: function(successCallback, errorCallback, id) {
-        if (_fileInputs[id]) {
-            var sound = _fileInputs[id].sound;
-            var settings = _fileInputs[id].settings;
-            
-            sound.outgoingGain = settings.volume;
-            sound.seek(settings.delay);
-            sound.loopCount = null;
-            sound.start();
-            
-            successCallback();
-        } else {
-            handleError('file with id ' + id + ' is not loaded');  
-            errorCallback('file with id ' + id + ' is not loaded');
+        var res = NativeAudioRT.NativeAudioRTImpl.loop(id);
+        
+        if (res == 0) {
+            errorCallback(res);
+        }
+        else {
+            successCallback(res);
         }
     },
     
     stop: function(successCallback, errorCallback, id) {
-        if (_fileInputs[id]) {
-            var sound = _fileInputs[id].sound;
-            
-            sound.stop();
-            successCallback();
-        } else {
-            handleError('file with id ' + id + ' is not loaded');  
-            errorCallback('file with id ' + id + ' is not loaded');
-        }   
+        var res = NativeAudioRT.NativeAudioRTImpl.stop(id);
+        
+        if (res == 0) {
+            errorCallback(res);
+        }
+        else {
+            successCallback(res);
+        }
     },
     
     unload: function(successCallback, errorCallback, id) {
-        if (_fileInputs[id]) {
-            delete _fileInputs[id];
-            successCallback();
-        } else {
-            handleError('file with id ' + id + ' is not loaded');  
-            errorCallback('file with id ' + id + ' is not loaded');
+        var res = NativeAudioRT.NativeAudioRTImpl.unload(id);
+        
+        if (res == 0) {
+            errorCallback(res);
+        }
+        else {
+            successCallback(res);
         }       
     },
     
     setVolumeForComplexAsset: function(successCallback, errorCallback, id, volume) {
-        if (_fileInputs[id]) {
-            var sound = _fileInputs[id].sound;
+        var res = NativeAudioRT.NativeAudioRTImpl.setVolumeForComplexAsset(id, volume);
         
-            sound.outgoingGain = volume;
-            successCallback();
-        } else { 
-            handleError('file with id ' + id + ' is not loaded');  
-            errorCallback('file with id ' + id + ' is not loaded');
+        if (res == 0) {
+            errorCallback(res);
+        }
+        else {
+            successCallback(res);
         }
     }
 };
